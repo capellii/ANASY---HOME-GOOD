212de3d0280de9f44d3de618c80d996541689a5f
Victor Capelli <capelli_saofrancisco@outlook.com>
2026-01-27 21:34:45 -0300

    FEAT - PostgreSQL

--- ANASY/backend/src/app.ts
+++ ANASY/backend/src/app.ts
@@ -1,5 +1,7 @@
 
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import authRoutes from './routes/authRoutes';
 import deviceRoutes from './routes/deviceRoutes';
 import scenarioRoutes from './routes/scenarioRoutes';
