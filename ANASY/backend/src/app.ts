a9cde8a9f11aaffc9b9fad18b5c46062a1eab955
Victor Capelli <capelli_saofrancisco@outlook.com>
2026-01-27 21:04:45 -0300

    CHORE - Routes API

--- ANASY/backend/src/app.ts
+++ ANASY/backend/src/app.ts
@@ -1,4 +1,8 @@
+
 import express from 'express';
+import authRoutes from './routes/authRoutes';
+import deviceRoutes from './routes/deviceRoutes';
+import scenarioRoutes from './routes/scenarioRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +12,11 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('ANASY Backend API is running!');
 });
+
+// Rotas principais
+app.use('/api/auth', authRoutes);
+app.use('/api/devices', deviceRoutes);
+app.use('/api/scenarios', scenarioRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
