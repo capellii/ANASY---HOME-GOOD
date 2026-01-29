b1328393ed835db29209319fa4487e4acf3729c9
Victor Capelli <capelli_saofrancisco@outlook.com>
2026-01-29 16:11:25 -0300

    FEAT: x

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import DashboardScreen from './screens/DashboardScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
